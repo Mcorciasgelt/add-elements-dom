9aea60c2f80225f6afa367eda2204a7852bb1fd4
Michael Corcias <michael.corcias@gelt.com>
2024-11-05 23:11:30 +0100

    entregableU2S3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
  // Aquí tu código
+ const agregar = document.getElementById("agregar");
+ const lista = document.getElementById("lista");
+ 
+ agregar.addEventListener("click", function(){
+     elemento = prompt("Introduce un nuevo elemento a tu lista");
+     const elementoLista = document.createElement("li");
+     elementoLista.textContent = elemento;
+     lista.appendChild(elementoLista)
+
+  }) ;
